d73a79089d8457e011e8ffd4956b60ad41f141c2
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-09 23:37:07 +0100

    k

--- src/pages/product.tsx
+++ src/pages/product.tsx
@@ -1,4 +1,5 @@
 import { Reveal } from "@/components/Animations/Reveal";
+import { RevealLight } from "@/components/Animations/RevealLight";
 import Layout from "@/components/Layout";
 import Meta from "@/components/Layout/Meta";
 import MainHero from "@/components/Ui/MainHero";
@@ -8,9 +9,9 @@ export default function Pricing() {
     <Layout mode="">
       <Meta title="Pricing | ToolBird" />
       <MainHero heading="One tool to rule them all" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
-      <Reveal delayTime={0}>
-        <img loading="lazy" className="w-[90%] mx-auto shadow-clean rounded-[14px] mt-[-360px]" src="/showcases/waitlists.svg"></img>
-      </Reveal>
+      <RevealLight delayTime={0.3}>
+        <img className="w-[90%] mx-auto shadow-clean rounded-[14px] mt-[-360px]" src="/showcases/waitlists.svg"></img>
+      </RevealLight>
     </Layout>
   );
 }
