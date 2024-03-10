d9c42aa7ce3ea6df940e17fc8caa96b9a08e8b6a
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-10 19:48:46 +0100

    k

--- src/components/Ui/MainHero.tsx
+++ src/components/Ui/MainHero.tsx
@@ -3,12 +3,12 @@ import { Reveal } from "../Animations/Reveal";
 export default function MainHero(props:any) {
     return (
         <section className="bg-[url(/bgs/hero.svg)] bg-cover bg-center w-full pt-[170px] pb-[480px]">
-            <div className="text-white text-center w-[94%] mx-auto max-w-[1000px]">
+            <div className="text-white text-center w-[94%] mx-auto max-w-[1100px]">
                 <Reveal delayTime={0}>
-                    <h1 className="font-[300] tracking-tight text-[70px] mb-[14px]">{props.heading}</h1>
+                    <h1 className="font-[300] tracking-tight text-[70px] leading-[80px] mb-[14px]">{props.heading}</h1>
                 </Reveal>
                 <Reveal delayTime={0}>
-                    <p className="font-[200] text-[18px] w-[80%] mx-auto text-gray-200">{props.paragraph}</p>
+                    <p className="font-[200] text-[18px] w-[80%] mx-auto text-gray-200 max-w-[1000px]">{props.paragraph}</p>
                 </Reveal>
             </div>
         </section>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import Comparison from "@/components/comparison";
 export default function Home() {
   return (
     <Layout mode="">
-      <MainHero heading="The all-in-one toolkit for you SaaS" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
+      <MainHero heading="All the tools your SaaS needs to start, scale and run" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
       <Comparison />
     </Layout>
   );
