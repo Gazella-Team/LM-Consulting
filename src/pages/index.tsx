3cdbc7c9db05247f8a0f0b992caa3b1d80e739c8
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-22 11:56:07 +0100

    host

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,15 +7,16 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <section style={{backgroundImage: "url(/bgs/wave.svg)"}} className="h-screen bg-center bg-cover flex flex-col justify-center">
+    <section style={{backgroundImage: "url(/bgs/wave.svg)"}} className="min-h-screen bg-center bg-cover flex flex-col justify-center">
       <div className="flex w-[80%] mx-auto justify-center gap-10 flex-col">
         <Reveal delayTime={0.1}>
-          <h1 className="text-[60px] w-[50%] text-gray-800 leading-[65px] headingText">The all-in-one toolbox for tech startups<span className="text-main">.</span></h1>
+          <h1 className="text-[40px] leading-[45px] md:text-[60px] w-[94%] max-w-[600px] text-gray-800 md:leading-[65px] headingText">The all-in-one toolbox for tech startups<span className="text-main">.</span></h1>
         </Reveal>
         <Reveal delayTime={0.1}>
-          <p className="text-gray-600 w-[50%] text-xl font-regular">
-            Are you tired of having tons of expensive tool-subscriptions spread amongst countless providers? Say hello to ToolBird
-            ToolBird the the go-to platform for tech startups who seek an intuitive platform to manage essential tools and build workflows.
+          <p className="text-gray-600 w-[94%] max-w-[600px] font-regular text-[16px] md:text-xl">
+            Are you tired of having tons of expensive tool-subscriptions spread amongst countless providers? Say hello to ToolBird.
+            <br></br><br></br>
+            ToolBird is the the go-to platform for tech startups who seek an intuitive platform to manage essential tools and build workflows.
             <br></br><br></br>
             We have taken all the tools your tech startup needs to start, scale and run, and collected them all in one platform!
             <br></br><br></br>
@@ -23,8 +24,10 @@ export default function Home() {
           </p>
         </Reveal>
         <div className="flex items-center gap-4">
-          <p className="text-gray-600">Powered by Gazella Team</p>
-          <img className="w-[80px]" src="/gtlogogray.svg"></img>
+          <p className="text-gray-600">Powered by</p>
+          <Link target="_blank" href={"https://gazellateam.com/"}>
+            <img className="w-[80px]" src="/gtlogogray.svg"></img>
+          </Link>
         </div>
       </div>
     </section>
