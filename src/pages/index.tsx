6abd1be5f199e14ea0340f003e967308dfd9a58b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-31 23:09:26 +0200

    chjanges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,14 +31,6 @@ export default function Home() {
     <section className="bg-[url(/bg.svg)] bg-center bg-cover h-screen flex items-center justify-center">
       <div className="bg-white rounded-xl border border-gray-600/10 p-14 w-[94%] mx-auto max-w-[1000px] min-h-[580px] shadow-xl flex flex-col justify-between">
         <div>
-          <h1 className="text-2xl leading-8 headingText text-gray-800 mb-4">
-            Introducing ToolBird: The all-in-one toolbox for{" "}
-            <span className="headingText text-main">SaaS developers.</span>
-          </h1>
-          <p className="text-gray-600">
-            Tired of having countless tool-subscriptions spread amongst
-            countless providers? Say hello to ToolBird. We offer a platform
-            where SaaS developers can manage tools.
           <img className="w-[120px] mb-2" src="/logos/logograytext.svg"></img>
           <h1 className="text-4xl leading-[44px] max-w-[600px] headingText text-gray-800 mb-4">Introducing ToolBird: The ultimate toolbox for <span className="headingText text-main">SaaS developers.</span></h1>
           <p className="text-gray-600 max-w-[700px] mb-8">
@@ -77,14 +69,14 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
-          <input type="e-mail" className="bg-slate-50" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
-          <button onClick={() => handleSubmit()}>
+        <div className="flex gap-2 mt-16">
+          <input placeholder="example@toolbird.io" type="e-mail" className="bg-slate-50 border border-gray-600/10 rounded-md flex-1 placeholder:text-sm placeholder:pl-2" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
+          <button className="bg-main text-white px-4 py-2 text-sm font-medium rounded-md" onClick={() => handleSubmit()}>
             Join waitlist
           </button>
-          <p className="text-center text-sm mt-4 text-gray-400">Free usage for the first month. Limited spots.</p>
         </div>
-        <p className="text-center text-sm mt-0 text-gray-400">Free usage for the first month. Limited spots.</p>
+        <p className="text-center text-sm mt-4 text-gray-400">Free usage for the first month. Limited spots.</p>
+      </div>
       </div>
     </section>
   );
