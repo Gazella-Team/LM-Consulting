c1f57227e505f296f563c32111c464eb7fc80bd4
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-31 22:58:37 +0200

    ij

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,39 +18,55 @@ export default function Home() {
 
   return (
     <section className="bg-[url(/bg.svg)] bg-center bg-cover h-screen flex items-center justify-center">
-      <div className="bg-white rounded-xl border border-gray-600/10 p-14 w-[94%] mx-auto max-w-[500px] min-h-[600px] shadow-xl flex flex-col justify-between">
+      <div className="bg-white rounded-xl border border-gray-600/10 p-14 w-[94%] mx-auto max-w-[1000px] min-h-[580px] shadow-xl flex flex-col justify-between">
         <div>
-          <h1 className="text-2xl leading-8 headingText text-gray-800 mb-4">Introducing ToolBird: The all-in-one toolbox for <span className="headingText text-main">SaaS developers.</span></h1>
-          <p className="text-gray-600">
+          <img className="w-[120px] mb-2" src="/logos/logograytext.svg"></img>
+          <h1 className="text-4xl leading-[44px] max-w-[600px] headingText text-gray-800 mb-4">Introducing ToolBird: The ultimate toolbox for <span className="headingText text-main">SaaS developers.</span></h1>
+          <p className="text-gray-600 max-w-[700px] mb-8">
             Tired of having countless tool-subscriptions spread amongst countless providers? Say hello to ToolBird.
             We offer a platform where SaaS developers can manage tools.
           </p>
+          <div className="flex gap-10 font-medium text-[15px]">
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>All tools in one place</p>
+            </div>
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>Collaborate with your team</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>All tools in one place</p>
+            </div>
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>Collaborate with your team</p>
+            </div>
+          </div>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>All tools in one place</p>
+            </div>
+            <div className="flex items-center gap-2 text-main">
+              <CheckCircleIcon width={22} />
+              <p>Collaborate with your team</p>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 font-medium text-[15px]">
-          <div className="flex items-center gap-2 text-main">
-            <CheckCircleIcon width={22} />
-            <p>All tools in one place</p>
-          </div>
-          <div className="flex items-center gap-2 text-main">
-            <CheckCircleIcon width={22} />
-            <p>Collaborate with your team</p>
-          </div>
-          <div className="flex items-center gap-2 text-main">
-            <CheckCircleIcon width={22} />
-            <p>Easy integration with your code</p>
-          </div>
-          <div className="flex items-center gap-2 text-main">
-            <CheckCircleIcon width={22} />
-            <p>Flexible fee-free pricing</p>
-          </div>
         </div>
-        <div>
-          <input type="e-mail" className="bg-slate-50" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
-          <button onClick={() => handleSubmit()}>
+        
+        <div className="flex gap-4">
+          <input type="e-mail" placeholder="lasse@toolbird.io" className="bg-slate-50 border placeholder:pl-3 placeholder:text-sm border-gray-600/10 rounded-md flex-1" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
+          <button className="bg-main text-white px-4 py-2 rounded-md text-sm font-medium" onClick={() => handleSubmit()}>
             Join waitlist
           </button>
-          <p className="text-center text-sm mt-4 text-gray-400">Free usage for the first month. Limited spots.</p>
         </div>
+        <p className="text-center text-sm mt-0 text-gray-400">Free usage for the first month. Limited spots.</p>
       </div>
     </section>
   )
