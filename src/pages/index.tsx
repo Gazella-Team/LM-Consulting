840ff00018bff03d1b3f9a63ab72463704708dd4
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-03-31 23:22:20 +0200

    chnges

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,12 +32,12 @@ export default function Home() {
       <div className="bg-white rounded-xl border border-gray-600/10 p-14 w-[94%] mx-auto max-w-[1000px] min-h-[580px] shadow-xl flex flex-col justify-between">
         <div>
           <img className="w-[120px] mb-2" src="/logos/logograytext.svg"></img>
-          <h1 className="text-4xl leading-[44px] max-w-[600px] headingText text-gray-800 mb-4">Introducing ToolBird: The ultimate toolbox for <span className="headingText text-main">SaaS developers.</span></h1>
-          <p className="text-gray-600 max-w-[700px] mb-8">
+          <h1 className="text-2xl leading-[30px] md:text-4xl md:leading-[44px] max-w-[600px] headingText text-gray-800 mb-4">Introducing ToolBird: The ultimate toolbox for <span className="headingText text-main">SaaS developers.</span></h1>
+          <p className="text-gray-600 text-sm md:text-base max-w-[700px] mb-8">
             Tired of having countless tool-subscriptions spread amongst countless providers? Say hello to ToolBird.
             We offer a platform where SaaS developers can manage tools.
           </p>
-          <div className="flex gap-10 font-medium text-[15px]">
+          <div className="hidden md:flex gap-10 font-medium text-[15px]">
           <div className="flex flex-col gap-4">
             <div className="flex items-center gap-2 text-main">
               <CheckCircleIcon width={22} />
@@ -70,12 +70,12 @@ export default function Home() {
           </div>
         </div>
         <div className="flex gap-2 mt-16">
-          <input placeholder="example@toolbird.io" type="e-mail" className="bg-slate-50 border border-gray-600/10 rounded-md flex-1 placeholder:text-sm placeholder:pl-2" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
+          <input placeholder="example@toolbird.io" type="e-mail" className="bg-slate-50 text-sm pl-2 text-gray-800 border border-gray-600/10 rounded-md flex-1 placeholder:text-sm" value={email} onChange={(value) => setEmail(value.currentTarget.value)}></input>
           <button className="bg-main text-white px-4 py-2 text-sm font-medium rounded-md" onClick={() => handleSubmit()}>
             Join waitlist
           </button>
         </div>
-        <p className="text-center text-sm mt-4 text-gray-400">Free usage for the first month. Limited spots.</p>
+        <p className="text-center text-sm mt-4 text-gray-400">Free usage for the first month. Limited spots.<br></br><span>Powered by Gazella Team</span></p>
       </div>
       </div>
     </section>
