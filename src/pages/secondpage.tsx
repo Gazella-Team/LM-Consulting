382caed5d4fea5200a6e808f6e9851d1748b9eb4
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-04 14:18:41 +0200

    s

--- src/pages/secondpage.tsx
+++ src/pages/secondpage.tsx
@@ -1,9 +1,19 @@
-import Layout from "@/components/Layout"
+import localFont from "next/font/local"
+
+const GazellaTeamFont = localFont({
+  src: [
+    {
+      path: '../fonts/gelix-semibold.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+  ],
+})
 
 export default function SecondPage() {
     return (
-        <Layout mode="">
-            <p className="text-8xl">A place for creators and collectors.</p>
-        </Layout>
+        <section className={GazellaTeamFont.style}>
+            <p className="text-8xl text-center py-40 tracking-tight w-[70%] mx-auto text-gray-800">The ultimate toolbox for <span className="text-main">SaaS developers</span></p>
+        </section>
     )
 }
