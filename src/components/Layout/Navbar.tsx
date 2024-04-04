37a8fad3ff18f3e82bad37ee19c2404a2e13b330
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-04 22:04:15 +0200

    changes

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
                 </div>
                 <div className="w-[20%] flex items-center justify-end gap-5">
                     <Link target="_blank" href={"https://app.toolbird.io/auth/login"}>Log in</Link>
-                    <Link className="bg-main border-[3px] border-main text-sm text-white px-6 py-2 rounded-full" href={"/waitlist"}>Join waitlist</Link>
+                    <Link className="bg-main border-[3px] border-main text-white px-6 py-1 rounded-full" href={"/waitlist"}>Join waitlist</Link>
                 </div>
             </div>
         </nav>
--- src/components/pricingtable.tsx
+++ src/components/pricingtable.tsx
@@ -1,11 +1,13 @@
 import { CheckBadgeIcon, XCircleIcon } from "@heroicons/react/20/solid"
+import { FireIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
+import { GiFlame } from "react-icons/gi"
 
 const pricingData = [
     {
         planName: "Premium",
-        planPrice: "$49",
-        paymentRuntime: "per month",
+        planPrice: "$0",
+        paymentRuntime: "free",
         planTarget: "Perfect for pre-seed",
         cta: "Upgrade",
         tools: [
@@ -25,7 +27,7 @@ const pricingData = [
     },
     {
         planName: "Premium",
-        planPrice: "$49",
+        planPrice: "$12",
         paymentRuntime: "per month",
         planTarget: "Perfect for pre-seed",
         cta: "Upgrade",
@@ -47,7 +49,7 @@ const pricingData = [
     },
     {
         planName: "Premium",
-        planPrice: "$49",
+        planPrice: "$39",
         paymentRuntime: "per month",
         planTarget: "Perfect for pre-seed",
         cta: "Upgrade",
@@ -104,7 +106,7 @@ const PricingCard = (props:any) => {
         <div className="border border-gray-600/10 rounded-xl text-gray-800">
             <div className="border-b border-gray-600/10 p-6 py-4 flex items-center gap-2 text-sm">
                 <p className="text-xl">{props.Obj.planName}</p>
-                {props.Obj.mostPopular && <p className="bg-slate-50 px-2 py-1 rounded-md border border-gray-600/10 text-main">most popular</p>}
+                {props.Obj.mostPopular && <p className="bg-slate-50 px-2 py-1 rounded-md border border-gray-600/10 text-main font-[400] flex items-center gap-1"><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4"> <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0 1 12 21 8.25 8.25 0 0 1 6.038 7.047 8.287 8.287 0 0 0 9 9.601a8.983 8.983 0 0 1 3.361-6.867 8.21 8.21 0 0 0 3 2.48Z" /> <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 0 0 .495-7.468 5.99 5.99 0 0 0-1.925 3.547 5.975 5.975 0 0 1-2.133-1.001A3.75 3.75 0 0 0 12 18Z" /> </svg></span>Most Popular</p>}
             </div>
             <div className="p-6 flex flex-col gap-4">
                 <div className="mb-2">
@@ -121,9 +123,8 @@ const PricingCard = (props:any) => {
             </div>
             <div className="border-t border-gray-600/10">
                 <div className="p-6">
-                    <h2 className="mb-4 font-[500] uppercase">Tools</h2>
                     <div className="flex flex-col gap-4">
-                        {props.Obj.tools.map((v:any) => <div key={v} className="flex items-center justify-between text-gray-600 border-b border-gray-600/10 pb-4">{v[0]}{v[1] ? <CheckBadgeIcon width={20} />:<XCircleIcon width={20} />}</div>)}
+                        {props.Obj.tools.map((v:any, index:number) => <div key={v} className={index + 1 === props.Obj.tools.length ? "flex items-center justify-between text-gray-600":"flex items-center justify-between text-gray-600 border-b border-gray-600/10 pb-4"}>{v[0]}{v[1] ? <CheckBadgeIcon width={20} />:<XCircleIcon width={20} />}</div>)}
                     </div>
                 </div>
             </div>
