627cf6b5d8ab546496373194e9bf633597c91110
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-05 16:58:32 +0200

    delayfix

--- src/components/dashboarddisplay.tsx
+++ src/components/dashboarddisplay.tsx
@@ -2,7 +2,7 @@ import { Reveal } from "./Animations/Reveal";
 
 export default function DashboardDisplay() {
     return (
-        <section className="pt-[70px] bg-cover">
+        <section className="pt-[70px] h-[800px] bg-cover">
             <div className="w-[96%] mx-auto max-w-[1200px]">
                 <Reveal delayTime={0.5}>
                     <img className="rounded-xl z-[0]" src="/showcases/demo.png"></img>
