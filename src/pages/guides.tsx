4440fccd65a107d0c800abb15fb9a872af24e54e
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-07 13:33:20 +0200

    hey

--- src/pages/guides.tsx
+++ src/pages/guides.tsx
@@ -3,7 +3,7 @@ import PageIntro from "@/components/PageIntro";
 
 export default function Guides() {
     return (
-        <Layout>
+        <Layout white={true}>
             <PageIntro hook="🥳 A new way of learning" highlighted="ToolBird Guides" heading="Introducing" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
         </Layout>
     )
--- src/pages/waitlist.tsx
+++ src/pages/waitlist.tsx
@@ -4,7 +4,7 @@ import PricingTable from "@/components/pricingtable";
 
 export default function Pricing() {
     return (
-        <Layout>
+        <Layout white={true}>
             <PageIntro hook="🌍 Early access" highlighted="demo version" heading="Limited spots: Get free early access to our" paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy." />
             <PricingTable />
         </Layout>
