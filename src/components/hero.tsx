70f639483243a8ff1c0340b336500375125abc04
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-08 23:37:59 +0200

    hero

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -7,21 +7,14 @@ import { RevealLight } from "./Animations/RevealLight";
 export default function Hero() {
     return (
         <section className="py-[100px] pb-[0px]">
-        <div className="w-[96%] mx-auto max-w-[1300px] flex flex-col gap-6 text-center">
-            <RevealLight delayTime={0.4}>
-              <div>
-                <div className="inline-block text-sm mx-auto bg-slate-50 py-2 text-gray-600 px-6 rounded-full border border-gray-600/10">
-                  <p>⚡️ A Gazella Team product</p>
-                </div>
-              </div>
-            </RevealLight>
+        <div className="w-[96%] mx-auto max-w-[1300px] flex flex-col gap-5 text-center">
           <Reveal delayTime={0.1}>
-            <h1 className="text-[50px] md:text-[60px] xl:text-[70px] leading-[100%] font-semibold text-gray-800 heading">
-            ToolBird
+            <h1 className="text-[40px] md:text-[50px] xl:text-[60px] leading-[100%] font-bold text-gray-800 heading">
+            Empowering SaaS developers
             </h1>
           </Reveal>
           <Reveal delayTime={0.2}>
-            <p className="text-base md:text-lg xl:text-xl font-light max-w-[870px] mx-auto text-gray-600">
+            <p className="text-base md:text-lg xl:text-xl font-regular max-w-[870px] mx-auto text-gray-600">
             Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy.
             </p>
           </Reveal>
