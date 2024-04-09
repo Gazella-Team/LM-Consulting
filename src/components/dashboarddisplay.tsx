d9ed339649d2ad668d33c933ad98a79559a4a9ae
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-09 20:30:54 +0200

    landning

--- src/components/dashboarddisplay.tsx
+++ src/components/dashboarddisplay.tsx
@@ -4,7 +4,7 @@ export default function DashboardDisplay() {
     return (
         <section className="bg-[url(/bgs/line.svg)] bg-center bg-cover py-[100px]">
             <Reveal delayTime={0.5}>
-                <img className="w-[70%] mx-auto rounded-xl shadow-clean border border-gray-600/10" src="/showcases/dash.webp"></img>
+                <img className="w-[95%] max-w-[1100px] mx-auto rounded-xl shadow-clean border border-gray-600/10" src="/showcases/dash.webp"></img>
             </Reveal>
         </section>
     )
