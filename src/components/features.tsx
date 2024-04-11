cbecabd4ed2739c35d4419f8335d9e90870ee853
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-04-11 21:06:32 +0200

    new

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -8,7 +8,7 @@ export default function Features() {
                 <div className="bg-[url(/toolbgs/waitlists.svg)] bg-center bg-cover border border-gray-600/10 h-[500px] rounded-[30px] py-10 px-14">
                     <img className="w-32 mb-10" src="/toolicons/waitlists.svg"></img>
                     <h1 className="font-semibold text-3xl text-gray-800 max-w-[400px] mb-6">Create waitlists effortlessly with only 2 clicks</h1>
-                    <p className="max-w-[400px] text-gray-600">Collecting signups for your waitlist can be challenging. We've collected the top 10 best advice on how to build a packed list of signups for your coming waitlist</p>
+                    <p className="max-w-[400px] text-gray-600">Collecting signups for your waitlist can be challenging. Weve collected the top 10 best advice on how to build a packed list of signups for your coming waitlist</p>
                 </div>
             </div>
         </section>
