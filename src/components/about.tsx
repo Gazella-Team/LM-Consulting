88659998a0f952c03a6648b16a159ba9bf7e4b5e
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-06 08:20:32 +0200

    comi

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,6 +1,6 @@
 export default function About() {
     return (
-        <div className="text-center text-gray-800 mb-14 max-w-3xl mx-auto">
+        <div className="text-center text-gray-800 mb-14 max-w-xl mx-auto">
             <div className="flex items-center justify-center mb-10">
                 <img className="w-20 h-20 object-cover rounded-full border-2 border-main" src="/people/lasseosmann.webp"></img>
                 <img className="w-20 h-20 object-cover rounded-full border-2 border-main ml-[-20px]" src="/people/simonmaribo.webp"></img>
