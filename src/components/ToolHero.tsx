0822175f46884c6037fc7f0acd8df0cc865b0879
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-06 09:17:09 +0200

    cpmmit

---
+++ src/components/ToolHero.tsx
@@ -0,0 +1,15 @@
+type ToolHeroProps = {
+    heading:string,
+    description:string,
+    image:string,
+}
+
+export default function ToolHero(props:ToolHeroProps) {
+    return (
+        <section>
+            <div className="max-w-8xl bg-slate-50 mx-auto grid grid-cols-1 md:grid-cols-2">
+
+            </div>
+        </section>
+    )
+}
