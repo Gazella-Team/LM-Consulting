5ebf184e77c2c4d3ef8e3de501e9da2515d02594
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-09 11:52:25 +0200

    affiliate

---
+++ src/pages/affiliate.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/Layout'
+import PageIntro from '@/components/PageIntro'
+
+export default function Affiliate() {
+	return (
+		<Layout white={true}>
+			<PageIntro
+				hook="Affiliate"
+				highlighted="SaaS developers"
+				heading="Earn money promoting the #1 toolbox for"
+				paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere in seconds. Get more visits, convert more, and close more deals. Senja makes it easy."
+			/>
+		</Layout>
+	)
+}
