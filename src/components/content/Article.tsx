5f7c27e2cfd1500f634a6d8e06275415f9bf5de0
Simon Maribo <simonmaribo@outlook.dk>
2024-05-15 21:12:54 +0200

    F

--- src/components/content/Article.tsx
+++ src/components/content/Article.tsx
@@ -7,16 +7,10 @@ import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Mdx } from '@/components/content/mdx'
 import { format } from 'date-fns'
 import { ArrowLeft } from 'lucide-react'
+import { AuthorAvatars } from './blog/AuthorAvatars'
+import { AuthorNames } from './blog/AuthorNames'
 
 export function Article({ post }: { post: Post }) {
-	const getNameInitials = (name: string) => {
-		const individualNames = name.split(' ')
-		return (
-			individualNames[0][0] +
-			individualNames[individualNames.length - 1][0]
-		)
-	}
-
 	return (
 		<article className="border border-gray-600/10 relative mx-auto flex max-w-6xl w-[90%] p-8 rounded-xl bg-white flex-col gap-8">
 			<div className="grid max-w-3xl mx-auto w-full gap-3">
@@ -30,38 +24,23 @@ export function Article({ post }: { post: Post }) {
 					</Link>
 				</div>
 				<h1 className="font-semibold text-3xl">{post.title}</h1>
-				<p className="text-sm font-light">
-					{format(new Date(post.publishedAt), 'MMMM dd, yyyy')}
-					<span className="text-gray-500 mx-1">&bull;</span>
-					{post.readingTime}
-				</p>
+				<h2 className="text-gray-500">{post.description}</h2>
 				<div className="flex items-center gap-3 mb-5">
-					<div className="flex gap-4">
-						{post.authors.map((author, index) => (
-							<div
-								key={index}
-								className="flex items-center gap-2"
-							>
-								<Avatar>
-									<AvatarImage src={author.avatar} />
-									<AvatarFallback>
-										{getNameInitials(author.name)}
-									</AvatarFallback>
-								</Avatar>
-								<div>
-									<Link
-										href={author.url ?? '#'}
-										target="_blank"
-										className="text-sm cursor-pointer font-medium hover:underline"
-									>
-										{author.name}
-									</Link>
-									<p className="text-sm text-gray-700">
-										Technical Cofounder
-									</p>
-								</div>
-							</div>
-						))}
+					<div className="flex gap-2">
+						<AuthorAvatars authors={post.authors} />
+						<div>
+							<AuthorNames authors={post.authors} />
+							<p className="text-sm text-gray-500 font-light">
+								{format(
+									new Date(post.publishedAt),
+									'MMMM dd, yyyy'
+								)}
+								<span className="text-gray-500 mx-1">
+									&bull;
+								</span>
+								{post.readingTime}
+							</p>
+						</div>
 					</div>
 				</div>
 				<div className="border-border relative h-full w-full overflow-hidden rounded-lg border">
