3a2a270488f88aaa9161cafad37b7a6a965c0cf2
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-18 13:19:52 +0200

    new

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from 'tailwindcss'
 const svgToDataUri = require('mini-svg-data-uri')
-const { blackA, mauve, violet, indigo, purple } = require('@radix-ui/colors');
-const plugin = require('tailwindcss/plugin');
 
 const {
 	default: flattenColorPalette,
@@ -34,50 +32,10 @@ const config = {
 					from: { height: 'var(--radix-accordion-content-height)' },
 					to: { height: '0' },
 				},
-				enterFromRight: {
-					from: { opacity: '0', transform: 'translateX(200px)' },
-					to: { opacity: '1', transform: 'translateX(0)' },
-				  },
-				  enterFromLeft: {
-					from: { opacity: '0', transform: 'translateX(-200px)' },
-					to: { opacity: '1', transform: 'translateX(0)' },
-				  },
-				  exitToRight: {
-					from: { opacity: '1', transform: 'translateX(0)' },
-					to: { opacity: '0', transform: 'translateX(200px)' },
-				  },
-				  exitToLeft: {
-					from: { opacity: '1', transform: 'translateX(0)' },
-					to: { opacity: '0', transform: 'translateX(-200px)' },
-				  },
-				  scaleIn: {
-					from: { opacity: '0', transform: 'rotateX(-10deg) scale(0.9)' },
-					to: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
-				  },
-				  scaleOut: {
-					from: { opacity: '1', transform: 'rotateX(0deg) scale(1)' },
-					to: { opacity: '0', transform: 'rotateX(-10deg) scale(0.95)' },
-				  },
-				  fadeIn: {
-					from: { opacity: '0' },
-					to: { opacity: '1' },
-				  },
-				  fadeOut: {
-					from: { opacity: '1' },
-					to: { opacity: '0' },
-				  },
 			},
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
-				scaleIn: 'scaleIn 200ms ease',
-				scaleOut: 'scaleOut 200ms ease',
-				fadeIn: 'fadeIn 200ms ease',
-				fadeOut: 'fadeOut 200ms ease',
-				enterFromLeft: 'enterFromLeft 250ms ease',
-				enterFromRight: 'enterFromRight 250ms ease',
-				exitToLeft: 'exitToLeft 250ms ease',
-				exitToRight: 'exitToRight 250ms ease',
 			},
 			backgroundImage: {
 				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
@@ -86,11 +44,6 @@ const config = {
 			},
 			colors: {
 				main: '#5E17EB',
-				...blackA,
-				...mauve,
-				...violet,
-				...purple,
-				...indigo,
 			},
 			boxShadow: {
 				clean: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
