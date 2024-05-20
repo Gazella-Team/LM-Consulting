bc6c0b5cbaaa6702d38649b51c90a153cd60583b
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-20 16:50:43 +0200

    new

---
+++ src/components/ui/Tooltip/index.tsx
@@ -0,0 +1,36 @@
+import { cn } from '@/helpers/utils'
+import {
+	TooltipProvider,
+	Tooltip as RadixTooltip,
+	TooltipTrigger,
+	TooltipContent,
+} from './provider'
+
+export default function Tooltip({
+	content,
+	jsx,
+	children,
+	className,
+	style,
+}: {
+	content?: string
+	jsx?: React.ReactNode
+	children: React.ReactNode
+	className?: string
+	style?: React.CSSProperties
+}) {
+	if (!content && !jsx) return <>{children}</>
+	return (
+		<TooltipProvider>
+			<RadixTooltip delayDuration={100}>
+				<TooltipTrigger>{children}</TooltipTrigger>
+				<TooltipContent className={cn(className)} style={style}>
+					{content && (
+						<p className={cn(className, "font-medium max-w-[300px]")}>{content}</p>
+					)}
+					{jsx && jsx}
+				</TooltipContent>
+			</RadixTooltip>
+		</TooltipProvider>
+	)
+}
