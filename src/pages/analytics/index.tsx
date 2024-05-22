c2f4a5dac7ee9d3df348d141043a5224121befef
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-05-22 22:03:07 +0200

    ew

--- src/pages/analytics/index.tsx
+++ src/pages/analytics/index.tsx
@@ -7,17 +7,12 @@ import Features from '@/components/toolpage/Features'
 import { ChartBarIcon } from '@heroicons/react/24/outline'
 import DashboardDisplay from '@/components/dashboarddisplay'
 import BentoGridComponent from '@/components/bento-grids/analytics-bento-grid'
+import Hero02 from '@/components/hero-02'
 
 export default function Analytics() {
 	return (
 		<Layout white={true}>
-			<PageIntro
-				hook="Analytics"
-				highlighted="your traffic"
-				heading="Privacy friendly website analytics of"
-				paragraph="Wake up to an inbox full of new testimonial alerts. Share them everywhere."
-			/>
-			<DashboardDisplay src={"/showcases/analysis.webp"} />
+			<Hero02 />
 			<SectionIntro bottomMargin={true} heading='Powerful insights' description='Discover, collaborate, and grow, our all-in-one platform empowers you to build.' hook='Features Included'>
 				<BentoGridComponent />
 			</SectionIntro>
