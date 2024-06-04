c6424c0cb643d8ddab446fc3eda5cc08411a9433
Simon Maribo <simonmaribo@outlook.dk>
2024-06-04 10:46:37 +0200

    Pricing FSQ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,15 @@
 import Layout from '@/components/Layout'
 import SectionIntro from '@/components/SectionIntro'
 import BentoGridComponent from '@/components/bento-grids/landing-bento-grid'
-import BlurDashboard from '@/components/blur-dashboard'
-import BlurSection from '@/components/blur-section'
-import CustomerSlider from '@/components/customer-slider'
 import DashboardDisplay from '@/components/dashboarddisplay'
-import FAQ from '@/components/faq'
 import Hero from '@/components/hero'
-import Hero02 from '@/components/hero-02'
-import Testimonials from '@/components/testimonials'
 
 export default function Home() {
 	return (
 		<Layout white={true}>
 			<Hero />
-			<DashboardDisplay src='/showcases/platform.webp'  />
-			<div className='mt-[-100px]'>
+			<DashboardDisplay src="/showcases/platform.webp" />
+			<div className="mt-[-100px]">
 				<SectionIntro
 					heading="The all-in-one cockpit for your SaaS startup"
 					description="With our all-in-one platform we ensure better overview and higher productivity"
