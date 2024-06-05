57dcf14a76f2bb100a4f46e130c95118281cc9d3
Simon Maribo <simonmaribo@outlook.dk>
2024-06-05 10:42:17 +0200

    b

--- src/pages/legal/terms.tsx
+++ src/pages/legal/terms.tsx
@@ -1,39 +1,100 @@
 import Layout from '@/components/Layout'
-import PageIntro from '@/components/PageIntro'
-import ToolHero from '@/components/toolpage/ToolHero'
-import PricingTable from '@/components/pricingtable'
-import SectionIntro from '@/components/SectionIntro'
-import Features from '@/components/toolpage/Features'
-import { ChartBarIcon } from '@heroicons/react/24/outline'
-import DashboardDisplay from '@/components/dashboarddisplay'
-import BentoGridComponent from '@/components/bento-grids/analytics-bento-grid'
-import BlurSection from '@/components/blur-section'
-import BlurDashboard from '@/components/blur-dashboard'
 
 export default function Terms() {
 	return (
 		<Layout white={true}>
-			<div className='w-[86%] mx-auto max-w-6xl py-32 text-center'>
-				<h1 className='text-gray-800 text-6xl font-semibold'>Terms of Service</h1>
+			<div className="w-[86%] mx-auto max-w-6xl py-32 text-center">
+				<h1 className="text-gray-800 text-6xl font-semibold">
+					Terms of Service
+				</h1>
 			</div>
-			<section className='border-y paragraph border-gray-600/10 py-16' style={{background: "linear-gradient(329deg, rgba(244,244,244,1) 0%, rgba(250,248,255,1) 17%, rgba(247,247,247,1) 37%, rgba(253,252,255,1) 67%, rgba(252,251,255,1) 94%)"}}>
-				<div className='mx-auto max-w-xl flex flex-col gap-14 leading-8 text-gray-600'>
-					<p>Welcome to Dub.co (the "Site"), hosted by Dub ("Dub", "we", "us", and/or "our"). Dub provides a platform for creating and managing short links (the "Services")​1​. We value your privacy and are dedicated to protecting your personal data. This Privacy Policy covers how we collect, handle, and disclose personal data on our Platform. If you have any questions, comments, or concerns regarding this Privacy Policy, our data practices, or would like to exercise your rights, do not hesitate to contact us.</p>
-					<div className='flex flex-col gap-5'>
-						<h2 className='text-xl font-semibold text-gray-800'>What We Use Your Information For</h2>
-						<p>Welcome to Dub.co (the "Site"), hosted by Dub ("Dub", "we", "us", and/or "our"). Dub provides a platform for creating and managing short links (the "Services")​1​. We value your privacy and are dedicated to protecting your personal data. This Privacy Policy covers how we collect, handle, and disclose personal data on our Platform. If you have any questions, comments, or concerns regarding this Privacy Policy, our data practices, or would like to exercise your rights, do not hesitate to contact us.</p>
+			<section
+				className="border-y paragraph border-gray-600/10 py-16"
+				style={{
+					background:
+						'linear-gradient(329deg, rgba(244,244,244,1) 0%, rgba(250,248,255,1) 17%, rgba(247,247,247,1) 37%, rgba(253,252,255,1) 67%, rgba(252,251,255,1) 94%)',
+				}}
+			>
+				<div className="mx-auto max-w-xl flex flex-col gap-14 leading-8 text-gray-600">
+					<p>
+						Welcome to Dub.co (the "Site"), hosted by Dub ("Dub",
+						"we", "us", and/or "our"). Dub provides a platform for
+						creating and managing short links (the "Services")​1​.
+						We value your privacy and are dedicated to protecting
+						your personal data. This Privacy Policy covers how we
+						collect, handle, and disclose personal data on our
+						Platform. If you have any questions, comments, or
+						concerns regarding this Privacy Policy, our data
+						practices, or would like to exercise your rights, do not
+						hesitate to contact us.
+					</p>
+					<div className="flex flex-col gap-5">
+						<h2 className="text-xl font-semibold text-gray-800">
+							What We Use Your Information For
+						</h2>
+						<p>
+							Welcome to Dub.co (the "Site"), hosted by Dub
+							("Dub", "we", "us", and/or "our"). Dub provides a
+							platform for creating and managing short links (the
+							"Services")​1​. We value your privacy and are
+							dedicated to protecting your personal data. This
+							Privacy Policy covers how we collect, handle, and
+							disclose personal data on our Platform. If you have
+							any questions, comments, or concerns regarding this
+							Privacy Policy, our data practices, or would like to
+							exercise your rights, do not hesitate to contact us.
+						</p>
 					</div>
-					<div className='flex flex-col gap-5'>
-						<h2 className='text-xl font-semibold text-gray-800'>What We Use Your Information For</h2>
-						<p>Welcome to Dub.co (the "Site"), hosted by Dub ("Dub", "we", "us", and/or "our"). Dub provides a platform for creating and managing short links (the "Services")​1​. We value your privacy and are dedicated to protecting your personal data. This Privacy Policy covers how we collect, handle, and disclose personal data on our Platform. If you have any questions, comments, or concerns regarding this Privacy Policy, our data practices, or would like to exercise your rights, do not hesitate to contact us.</p>
+					<div className="flex flex-col gap-5">
+						<h2 className="text-xl font-semibold text-gray-800">
+							What We Use Your Information For
+						</h2>
+						<p>
+							Welcome to Dub.co (the "Site"), hosted by Dub
+							("Dub", "we", "us", and/or "our"). Dub provides a
+							platform for creating and managing short links (the
+							"Services")​1​. We value your privacy and are
+							dedicated to protecting your personal data. This
+							Privacy Policy covers how we collect, handle, and
+							disclose personal data on our Platform. If you have
+							any questions, comments, or concerns regarding this
+							Privacy Policy, our data practices, or would like to
+							exercise your rights, do not hesitate to contact us.
+						</p>
 					</div>
-					<div className='flex flex-col gap-5'>
-						<h2 className='text-xl font-semibold text-gray-800'>What We Use Your Information For</h2>
-						<p>Welcome to Dub.co (the "Site"), hosted by Dub ("Dub", "we", "us", and/or "our"). Dub provides a platform for creating and managing short links (the "Services")​1​. We value your privacy and are dedicated to protecting your personal data. This Privacy Policy covers how we collect, handle, and disclose personal data on our Platform. If you have any questions, comments, or concerns regarding this Privacy Policy, our data practices, or would like to exercise your rights, do not hesitate to contact us.</p>
+					<div className="flex flex-col gap-5">
+						<h2 className="text-xl font-semibold text-gray-800">
+							What We Use Your Information For
+						</h2>
+						<p>
+							Welcome to Dub.co (the "Site"), hosted by Dub
+							("Dub", "we", "us", and/or "our"). Dub provides a
+							platform for creating and managing short links (the
+							"Services")​1​. We value your privacy and are
+							dedicated to protecting your personal data. This
+							Privacy Policy covers how we collect, handle, and
+							disclose personal data on our Platform. If you have
+							any questions, comments, or concerns regarding this
+							Privacy Policy, our data practices, or would like to
+							exercise your rights, do not hesitate to contact us.
+						</p>
 					</div>
-					<div className='flex flex-col gap-5'>
-						<h2 className='text-xl font-semibold text-gray-800'>What We Use Your Information For</h2>
-						<p>Welcome to Dub.co (the "Site"), hosted by Dub ("Dub", "we", "us", and/or "our"). Dub provides a platform for creating and managing short links (the "Services")​1​. We value your privacy and are dedicated to protecting your personal data. This Privacy Policy covers how we collect, handle, and disclose personal data on our Platform. If you have any questions, comments, or concerns regarding this Privacy Policy, our data practices, or would like to exercise your rights, do not hesitate to contact us.</p>
+					<div className="flex flex-col gap-5">
+						<h2 className="text-xl font-semibold text-gray-800">
+							What We Use Your Information For
+						</h2>
+						<p>
+							Welcome to Dub.co (the "Site"), hosted by Dub
+							("Dub", "we", "us", and/or "our"). Dub provides a
+							platform for creating and managing short links (the
+							"Services")​1​. We value your privacy and are
+							dedicated to protecting your personal data. This
+							Privacy Policy covers how we collect, handle, and
+							disclose personal data on our Platform. If you have
+							any questions, comments, or concerns regarding this
+							Privacy Policy, our data practices, or would like to
+							exercise your rights, do not hesitate to contact us.
+						</p>
 					</div>
 				</div>
 			</section>
