f82d9f1a1195b28f77c1ef7fcea2f261060b8055
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-07 15:49:18 +0200

    ne

--- src/components/feature-box.tsx
+++ src/components/feature-box.tsx
@@ -12,13 +12,13 @@ export default function FeatureBox() {
 			className='mt-14 py-24 max-w-7xl mx-auto lg:rounded-[100px] backdrop:blur-3xl border-y border-gray-600/10'
 		>
 			<div className="w-[86%] flex flex-col gap-24 mx-auto max-w-6xl">
-                <div className="grid grid-cols-[50%_1fr] gap-20">
+                <div className="lg:grid grid-cols-[50%_1fr] lg:gap-20 flex gap-4 flex-col">
                     <div className="flex flex-col gap-6">
                         <p className="paragraph uppercase text-main text-base font-semibold">Privacy-first web analytics</p>
-                        <h2 className="text-3xl font-semibold text-gray-800 max-w-lg">Analyze all the traffic going through your website with one intuitive dashboard</h2>
+                        <h2 className="text-3xl font-semibold text-gray-800">Analyze all the traffic going through your website with one intuitive dashboard</h2>
                     </div>
                     <div className="flex flex-col justify-center gap-6 paragraph">
-                        <p className="text-gray-600">Track what pages your visitors use, where they&apos;re located, what device they&apos;re using and from where they are referred</p>
+                        <p className="text-gray-600 max-w-sm lg:max-w-lg">Track what pages your visitors use, where they&apos;re located, what device they&apos;re using and from where they are referred</p>
                         <div className="flex">
 							<Link
 								href={'https://app.toolbird.io/auth/register'}
@@ -30,7 +30,7 @@ export default function FeatureBox() {
                     </div>
                 </div>
                 <img className="border rounded-2xl border-gray-600/10" src="/showcases/platform.webp"></img>
-                <div className="grid grid-cols-2 paragraph gap-x-32 gap-y-16 w-[90%] mx-auto">
+                <div className="grid grid-cols-1 lg:grid-cols-2 paragraph gap-x-32 gap-y-16 w-[90%] mx-auto">
                     <div className="flex gap-4">
                         <div className="w-10">
                             <CheckCircle2 fill="rgb(40 40 51)" className="text-white" size={30} />
