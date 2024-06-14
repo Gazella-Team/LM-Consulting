1e264059638c9684df8af07d43f59cfc11fa8250
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-14 10:56:03 +0200

    cookie

---
+++ src/components/Layout/CookiePopup.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { useState } from "react";
+
+export default function CookiePopup() {
+    const [popupVisible, setPopupVisible] = useState(true)
+    return (
+        popupVisible &&         <div className="border bottom-6 flex flex-col gap-6 left-6 shadow-clean p-6 z-[100] bg-white fixed shadow-main/20 border-gray-600/10 rounded-2xl text-gray-600">
+        <p className="w-[200px] paragraph text-sm">
+        We use cookies to improve your experience. You can opt out of certain cookies. Find out more in our <span className="text-gray-800 font-medium underline"><Link href={"/legal/privacy"}>privacy policy</Link></span>.
+        </p>
+        <div className="flex items-center gap-2 paragraph">
+            <button
+                onClick={() => setPopupVisible(false)}
+                className="bg-gray-800 border-2 border-gray-800 font-medium text-xs text-white px-4 py-1 rounded-full"
+                >
+                Continue
+            </button>
+            <button
+                onClick={() => setPopupVisible(false)}
+                className="bg-white border-2 border-gray-800 font-medium text-xs text-gray-800 px-4 py-1 rounded-full hover:bg-gray-800 hover:text-white transition-all"
+                >
+                Reject
+            </button>
+        </div>
+    </div>
+    )
+}
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,6 +2,7 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import localFont from 'next/font/local'
 import Meta from './Meta'
+import CookiePopup from './CookiePopup'
 
 type LayoutProps = {
 	className?: string
@@ -25,6 +26,7 @@ export default function Layout(props: LayoutProps) {
 		<div className="flex flex-col justify-between min-h-screen">
 			<Meta />
 			<div className="w-full bg-white bg-grid-black/[0.04] relative">
+				<CookiePopup />
 				<div className="absolute pointer-events-none inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 				<Navbar />
 				<div>
