82098ef3e6a1bc02bd0b018a504491aac2177c9b
Simon Maribo <simonmaribo@outlook.dk>
2024-06-18 13:40:43 +0200

    ADded UTM Page

---
+++ src/pages/tools/utm-generator.tsx
@@ -0,0 +1,15 @@
+import Layout from '@/components/Layout'
+
+export default function UTMGeneratorTool() {
+	return (
+		<Layout>
+			<section
+				style={{
+					background:
+						'linear-gradient(329deg, rgba(244,244,244,1) 0%, rgba(250,248,255,1) 17%, rgba(247,247,247,1) 37%, rgba(253,252,255,1) 67%, rgba(252,251,255,1) 94%)',
+				}}
+				className="mt-14 py-24 max-w-7xl mx-auto lg:rounded-[100px] backdrop:blur-3xl border-y border-gray-600/10"
+			></section>
+		</Layout>
+	)
+}
