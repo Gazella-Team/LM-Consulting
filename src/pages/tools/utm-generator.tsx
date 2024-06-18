8f1878599b03fa4f97d6eda49930f60c8f74e0bf
Simon Maribo <simonmaribo@outlook.dk>
2024-06-18 17:12:23 +0200

    F

--- src/pages/tools/utm-generator.tsx
+++ src/pages/tools/utm-generator.tsx
@@ -77,7 +77,7 @@ export default function UTMGeneratorTool() {
 						background:
 							'linear-gradient(329deg, rgba(244,244,244,1) 0%, rgba(250,248,255,1) 17%, rgba(247,247,247,1) 37%, rgba(253,252,255,1) 67%, rgba(252,251,255,1) 94%)',
 					}}
-					className="mt-14 py-24 px-12 max-w-7xl mx-auto rounded-3xl lg:rounded-[100px] backdrop:blur-3xl border-y border-gray-600/10"
+					className="mt-14 py-24 p-2 md:px-12 max-w-7xl mx-auto rounded-3xl lg:rounded-[100px] backdrop:blur-3xl border-y border-gray-600/10"
 				>
 					<div className="flex flex-col gap-12 mx-auto max-w-4xl">
 						<div className="flex flex-col gap-4">
