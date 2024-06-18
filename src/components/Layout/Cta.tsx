3a5dc27d0b728896a58b1ba5f69555b132849dbc
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-18 12:18:07 +0200

    bird-in-cta

--- src/components/Layout/Cta.tsx
+++ src/components/Layout/Cta.tsx
@@ -6,6 +6,11 @@ export default function Cta() {
 		<>
 		<section className="py-16 mt-24 pb-0 relative">
 			<div className="w-[86%] relative mx-auto max-w-6xl mb-20 text-center">
+				<div className='flex items-center justify-center mb-10'>
+					<div className='shadow-clean p-1 shadow-main/50 bg-white rounded-full border border-gray-600/10'>
+						<img className='w-12' src='/logos/birdlogo.png'></img>
+					</div>
+				</div>
 				<h2 className="text-[35px] md:text-[45px] xl:text-[50px] leading-[110%] max-w-[600px] mb-4 mx-auto font-extrabold text-gray-800">
 					<span className="bg-gradient-to-r from-main via-violet-700 to-main text-transparent bg-clip-text">
 						Supercharge{' '}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import Layout from '@/components/Layout'
 import CustomerSlider from '@/components/customer-slider'
 import AnalyticsFeatureBox from '@/components/feature-boxes/analytics-feature-box'
+import Hero from '@/components/hero'
 import Hero02 from '@/components/hero-02'
 
 export default function Home() {
 	return (
 		<Layout white={true}>
-			<Hero02 />
+			<Hero />
 			<div className="mt-[-130px]">
 				<AnalyticsFeatureBox />
 			</div>
