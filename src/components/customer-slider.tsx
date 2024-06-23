7ee0899fafbcc8b1210aa79d646db4df17c2ad4f
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-06-23 17:27:01 +0200

    changes

--- src/components/customer-slider.tsx
+++ src/components/customer-slider.tsx
@@ -2,168 +2,37 @@ import Link from 'next/link'
 import Slider from 'react-infinite-logo-slider'
 import { cn } from 'utils/cs'
 
-const imageData = [
+const testimonialData = [
 	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
+		name: "Andreas Pedersen",
+		testimonial: "Dub.co has been a game-changer for our marketing campaigns, our links get hundreds of thousands of clicks monthly and with Dub, we are able.",
+		image: "/users/user-headshots/andreas-pedersen.webp",
+		position: "Founder, Upboost.ai",
 	},
 	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
+		name: "Viktor Lasse",
+		testimonial: "Dub.co has been a game-changer for our marketing campaigns, our links get hundreds of thousands of clicks monthly and with Dub, we are able.",
+		image: "/users/user-headshots/andreas-pedersen.webp",
+		position: "Founder, MatematikTutor",
 	},
 	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
+		name: "Andreas Pedersen",
+		testimonial: "Dub.co has been a game-changer for our marketing campaigns, our links get hundreds of thousands of clicks monthly and with Dub, we are able.",
+		image: "/users/user-headshots/andreas-pedersen.webp",
+		position: "Founder, Upboost.ai",
 	},
 	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
+		name: "Viktor Lasse",
+		testimonial: "Dub.co has been a game-changer for our marketing campaigns, our links get hundreds of thousands of clicks monthly and with Dub, we are able.",
+		image: "/users/user-headshots/andreas-pedersen.webp",
+		position: "Founder, MatematikTutor",
 	},
 	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
+		name: "Viktor Lasse",
+		testimonial: "Dub.co has been a game-changer for our marketing campaigns, our links get hundreds of thousands of clicks monthly and with Dub, we are able.",
+		image: "/users/user-headshots/andreas-pedersen.webp",
+		position: "Founder, MatematikTutor",
 	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-	{
-		path: '/users/burst.svg',
-		website: 'https://burstcreators.com/',
-		size: 'w-[150px]',
-	},
-
 ]
 
 export default function CustomerSlider() {
@@ -172,23 +41,23 @@ export default function CustomerSlider() {
         <div className='flex items-center'>
           <Slider
             width="400px"
-            duration={200}
+            duration={30}
             pauseOnHover={false}
             blurBorders={true}
             blurBoderColor={"#fff"}
           >
-            {imageData.map((v, index) => (
-              <Slider.Slide key={v}>
+            {testimonialData.map((testimonial, index) => (
+              <Slider.Slide key={testimonial.name}>
 				<div style={index % 2 === 0 ? { background: 'linear-gradient(149deg, rgba(243,237,255,1) 0%, rgba(255,255,255,1) 50%)', }:{ background: 'linear-gradient(311deg, rgba(243,237,255,1) 0%, rgba(255,255,255,1) 50%)', }} className='flex shadow-clean mb-14 shadow-main/10 flex-col gap-10 w-[500px] text-gray-800 paragraph mr-10 border rounded-2xl border-gray-600/10 p-10 py-8'>
 					<p className='text-gray-600'>
-					&quot;Dub.co has been a game-changer for our marketing campaigns – our links get hundreds of thousands of clicks monthly and with Dub, we are able...&quot;
+					&quot;{testimonial.testimonial}&quot;
 					</p>
 					<div className='flex items-center justify-between'>
 						<div className='flex flex-col gap-[2px]'>
-							<h3 className="font-semibold">Andreas Pedersen</h3>
-							<p className='text-gray-600 font-normal text-sm'>Founder, Upboost.ai</p>
+							<h3 className="font-semibold">{testimonial.name}</h3>
+							<p className='text-gray-600 font-normal text-sm'>{testimonial.position}</p>
 						</div>
-						<img className='w-11 border border-gray-600/10 h-11 rounded-full' src='/users/user-headshots/andreas-pedersen.webp'></img>
+						<img className='w-11 border border-gray-600/10 h-11 rounded-full' src={testimonial.image}></img>
 					</div>
 				</div>
               </Slider.Slide>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,12 +9,16 @@ export default function Hero() {
 		<section className="py-24 pt-[60px] min-h-[900px] relative text-center">
 			<div className="w-[86%] mx-auto max-w-6xl relative flex flex-col gap-5">
 				<Reveal delayTime={0}>
-					<p className="font-[Caveat] text-main mb-4 text-2xl md:text-3xl">
-						Proudly built and launched in Europe
+					<p className="font-[Caveat] text-main mb-1 text-2xl md:text-3xl">
+						The all-in-one toolbox
 					</p>
 				</Reveal>
 				<Reveal delayTime={0.1}>
-					<h1 className="text-3xl md:text-4xl xl:text-6xl max-w-[1300px] mx-auto font-extrabold text-gray-800 xl:leading-[60px]">
+					<h1 className="text-5xl md:text-6xl xl:text-7xl max-w-[800px] mx-auto font-extrabold text-gray-800 xl:leading-[110%]">
+						Manage all user-insights in one place
+					</h1>
+					{/*
+					 					<h1 className="text-3xl md:text-4xl xl:text-6xl max-w-[1300px] mx-auto font-extrabold text-gray-800 xl:leading-[60px]">
 						Get{' '}
 						<span className="bg-main/10 text-gray-800 mb-2 md:mb-0 block md:inline-block p-2 py-1 rounded-xl">
 							Web Analytics,
@@ -28,19 +32,19 @@ export default function Hero() {
 						</span>{' '}
 						all in 1 place.
 					</h1>
+					 */}
 				</Reveal>
 				<Reveal delayTime={0.2}>
-					<p className="text-base md:text-lg mx-auto xl:text-xl font-regular paragraph mb-2 max-w-[800px] w-[85%] text-gray-600">
+					<p className="text-base md:text-lg mx-auto xl:text-xl font-regular paragraph mb-2 max-w-[500px] w-[85%] text-gray-600">
 						As a SaaS developer you no longer need to subscribe to
-						various pricy tool-providers to have the essential tools
-						needed to get an overview of your project.
+						various pricy tool-providers to have the essential tools.
 					</p>
 				</Reveal>
 				<RevealLight delayTime={0.4}>
 					<div className="flex items-center paragraph relative justify-center gap-3 mt-4 font-medium">
 						<Link
 							href="https://app.toolbird.io/auth/register"
-							className="bg-main border-[3px] border-main font-medium text-base text-white px-4 py-2 rounded-full"
+							className="bg-main border-[3px] border-main font-medium text-sm text-white px-4 py-2 rounded-full"
 						>
 Start 14-day free trial
 						</Link>
