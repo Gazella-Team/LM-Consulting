26c0974c2f4220767c3826b93f64cd1e6edfca9c
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-01 15:50:36 +0200

    heroes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,21 @@ import CustomerSlider from '@/components/customer-slider'
 import AnalyticsFeatureBox from '@/components/feature-boxes/analytics-feature-box'
 import Hero from '@/components/hero'
 import Hero02 from '@/components/hero-02'
+import Hero03 from '@/components/hero-03'
 import Mission from '@/components/mission'
 
 export default function Home() {
 	return (
 		<Layout white={true}>
-			<Hero />
-			<HeroDashboard />
+			<div className='hidden md:block'>
+				<Hero03>
+					<HeroDashboard />
+				</Hero03>
+			</div>
+			<div className='block md:hidden'>
+				<Hero />
+				<HeroDashboard />
+			</div>
 			<Mission />
 			<AnalyticsFeatureBox />
 			{/*
