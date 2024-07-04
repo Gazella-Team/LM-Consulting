a55579f7d33a0d87e3097833ae7d10f578cae00f
Simon Maribo <simonmaribo@outlook.dk>
2024-07-04 16:21:09 +0200

    F

--- src/components/HeroDashboard.tsx
+++ src/components/HeroDashboard.tsx
@@ -4,7 +4,7 @@ export default function HeroDashboard() {
 			<div className="w-[86%] mx-auto max-w-5xl">
 				<img
 					className="w-full bg-white shadow-clean shadow-main/40 rounded-3xl"
-					src="/showcases/platform.webp"
+					src="/showcases/sharing.png"
 				></img>
 			</div>
 		</section>
