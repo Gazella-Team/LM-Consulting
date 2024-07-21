9b82fcf019b466ae30aec5d337cadfce28836522
Lasse Osmann <84331862+lasse456@users.noreply.github.com>
2024-07-21 21:29:43 +0200

    new

---
+++ src/pages/03.tsx
@@ -0,0 +1,11 @@
+import { Reveal } from "@/components/Animations/Reveal";
+import { RevealLight } from "@/components/Animations/RevealLight";
+import { BlurHero } from "@/components/blur-hero";
+import Link from "next/link";
+
+export default function Design03() {
+    return (
+        <>
+        </>
+    )
+}
